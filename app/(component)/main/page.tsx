8e611272ef59341253c0fca232e048ffb202ce0c
Forinite <obefortune2@gmail.com>
2025-09-24 20:20:05 +0100

    Height fix commit

--- app/(component)/main/page.tsx
+++ app/(component)/main/page.tsx
@@ -64,7 +64,7 @@ const MainPage = () => {
                         key={short.id}
                         className="max-w-[400px] w-[90%] h-screen mx-auto flex items-center justify-center snap-start relative"
                     >
-                        <div className="h-[90%] w-full flex flex-col items-center justify-center border py-12 px-4 relative">
+                        <div className="h-[85vh] w-full flex flex-col items-center justify-center border py-12 px-4 relative">
                             {short.image && (
                                 <Image
                                     src={short.image}
